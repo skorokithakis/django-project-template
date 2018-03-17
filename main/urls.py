6bf3bb096cae02293545822a527b28d1190a9678
Stavros Korokithakis <hi@stavros.io>
2018-03-17 03:06:16 +0200

    Switch to new-style URL routes

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 """The main application's URLs."""
 
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-]  # yapf: disable
+    path(r'', views.index, name="index"),
+]
